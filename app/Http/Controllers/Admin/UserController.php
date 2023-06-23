ddfaddb858647935f2462900cefae5144ac36782
AKraja777 <ashokkumar7hitter@gmail.com>
2023-06-23 14:02:00 +0530

    usercontroller.php

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\CentralLogics\Helpers;
 use App\Http\Controllers\Controller;
 use App\Model\User;
+use App\Model\Comment;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
-
 class UserController extends Controller
 {
     public function index()

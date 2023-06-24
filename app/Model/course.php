bf1babb85a3dc96249a101215ddbd092b0ac686d
AKraja777 <ashokkumar7hitter@gmail.com>
2023-06-24 19:49:54 +0530

    course.php

--- app/Model/course.php
+++ app/Model/course.php
@@ -2,14 +2,14 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class course extends Model
 {
-    
-    protected $table='course';
+    protected $guarded = [];
 
+    public function category()
+    {
+        return $this->belongsTo(categories::class, 'category_id');
+    }
 }
-
-?>

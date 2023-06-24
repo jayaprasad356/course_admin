58ad28514c4771e45756205cd4867c42f891c37c
AKraja777 <ashokkumar7hitter@gmail.com>
2023-06-24 20:02:16 +0530

    coursecontroller.php

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -4,32 +4,27 @@ namespace App\Http\Controllers\Admin;
 
 use App\CentralLogics\Helpers;
 use App\Http\Controllers\Controller;
-use App\Model\course;
-use App\Model\categories;
+use App\Models\Course;
+use App\Models\Categories;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
-use Illuminate\Support\Facades\DB;
 
-class courseController extends Controller
+class CourseController extends Controller
 {
-   
-        public function index()
+    public function index()
     {
-        $categoriess = categories::all(); // Fetch all courses from the database
-        
+        $categories = Categories::all(); // Fetch all categories from the database
         return view('admin-views.course.index', compact('categories'));
     }
-    
+
     public function list(Request $request)
     {
         $query_param = [];
         $search = $request['search'];
         if ($request->has('search')) {
             $key = explode(' ', $request['search']);
-            $course = course::where(function ($q) use ($key) {
+            $courses = Course::where(function ($q) use ($key) {
                 foreach ($key as $value) {
                     $q->orWhere('name', 'like', "%{$value}%")
                         ->orWhere('sub_name', 'like', "%{$value}%")
@@ -41,22 +36,22 @@ class courseController extends Controller
             });
             $query_param = ['search' => $request['search']];
         } else {
-            $course = new course();
+            $courses = new Course();
         }
 
-        $course = $course->with('categories')->latest()->paginate(Helpers::getPagination())->appends($query_param);
+        $courses = $courses->with('categories')->latest()->paginate(Helpers::getPagination())->appends($query_param);
         return view('admin-views.course.list', compact('courses', 'search'));
     }
 
     public function preview($id)
     {
-        $course = course::findOrFail($id);
+        $course = Course::findOrFail($id);
         return view('admin-views.course.view', compact('course'));
     }
 
     public function store(Request $request)
     {
-        $course = new course();
+        $course = new Course();
         $course->image = Helpers::upload('course/', 'png', $request->file('image'));
         $course->name = $request->name;
         $course->category_id = $request->category_id;
@@ -68,14 +63,14 @@ class courseController extends Controller
 
     public function edit($id)
     {
-        $course = course::findOrFail($id);
-        $categories = categories::pluck('name', 'id'); // Fetch all categories as options for the dropdown
+        $course = Course::findOrFail($id);
+        $categories = Categories::pluck('name', 'id'); // Fetch all categories as options for the dropdown
         return view('admin-views.course.edit', compact('course', 'categories'));
     }
 
     public function update(Request $request, $id)
     {
-        $course = course::findOrFail($id);
+        $course = Course::findOrFail($id);
         $course->image = $request->has('image') ? Helpers::update('course/', $course->image, 'png', $request->file('image')) : $course->image;
         $course->name = $request->name;
         $course->status = $request->status;
@@ -88,7 +83,7 @@ class courseController extends Controller
 
     public function delete(Request $request)
     {
-        $course = course::findOrFail($request->id);
+        $course = Course::findOrFail($request->id);
         if (Storage::disk('public')->exists('course/' . $course['image'])) {
             Storage::disk('public')->delete('course/' . $course['image']);
         }

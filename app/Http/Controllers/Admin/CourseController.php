0e35aca0ef5c9ae67b15dc40fc60dba03983834f
AKraja777 <ashokkumar7hitter@gmail.com>
2023-06-26 13:05:36 +0530

    coursecontroller.php

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -42,12 +42,10 @@ class CourseController extends Controller
         } else {
             $course = Course::query();
         }
-
         $courses = $course->with('categories')->latest()->paginate(Helpers::getPagination())->appends($query_param);
-        $totalCourses = $courses->total();
-
         return view('admin-views.course.list', compact('courses', 'search'));
     }
+        
 
     public function preview($id)
     {

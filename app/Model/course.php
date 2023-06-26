bf49a6fb3546f3bf55942a7e6d98f2d510f825ad
AKraja777 <ashokkumar7hitter@gmail.com>
2023-06-26 13:02:21 +0530

    course.php

--- app/Model/course.php
+++ app/Model/course.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
+    protected $table = 'course'; // Specify the table name if it's different from the model name
+
     public function categories()
     {
         return $this->belongsTo(Categories::class, 'category_id');
